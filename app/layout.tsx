a795486c9be6a55199f717c66974b077b2cb1abf
buithienphat <146930716+buithienphat@users.noreply.github.com>
2024-08-20 14:03:37 +0700

    update website

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,8 @@ import "./globals.css";
 import Footer from "@/components/Footer";
 import PageTransition from "@/components/PageTransition";
 import StairTransition from "@/components/StairTransition";
+import Head from "next/head";
+import Script from "next/script";
 
 const jetbrainsMono = JetBrains_Mono({
   subsets: ["latin"],
@@ -36,7 +38,26 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head></head>
+      <Head>
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-WLBM66TP');`}
+        </Script>
+      </Head>
       <body className={jetbrainsMono.variable}>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WLBM66TP"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+
         <Header />
         <StairTransition />
         <PageTransition>
